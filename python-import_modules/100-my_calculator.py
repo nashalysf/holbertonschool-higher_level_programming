e5a3bb1c17d264f3363a9457ac70c3af339f835d
Nashalys Fernandez <nashalysf@gmail.com>
2023-06-03 19:25:40 -0400

    my calculator

---
+++ python-import_modules/100-my_calculator.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+# ask the user to enter two values and store them in variables
+num1, op, num2 = input("Calculator: ").split()
+
+# convert user input(str) into int
+num1 = int(num1)
+num2 = int(num2)
+
+# add
+if op == "+":
+    sum = num1 + num2
+    print(f"{num1} + {num2} = {sum}")
+# subtract
+elif op == "-":
+    sub = num1 - num2
+    print(f"{num1} - {num2} = {sub}")
+# mutiply
+elif op == "*":
+    mul = num1 * num2
+    print(f"{num1} * {num2} = {mul}")
+# divide
+elif op == "/":
+    div = num1 / num2
+    print(f"{num1} / {num2} = {div}")
+# reminder
+elif op == "%":
+    mod = num1 % num2
+    print(f"{num1} % {num2} = {mod}")
+# none
+else:
+    print(f"Please enter a valid operator: +  -  *  /  %")
