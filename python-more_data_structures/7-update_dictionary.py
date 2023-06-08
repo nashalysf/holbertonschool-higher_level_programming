38721075e41ffb1affccf3ec697e2b8519dafd47
Nashalys Fernandez <nashalysf@gmail.com>
2023-06-08 17:32:53 -0400

    fixing new value

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -2,5 +2,5 @@
 
 def update_dictionary(a_dictionary, key, value):
     new_dictionary = a_dictionary.copy()
-    new_dictionary.update({key: value})
+    new_dictionary[key] = value
     return (new_dictionary)
