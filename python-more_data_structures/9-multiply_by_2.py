6c89ad43f98a86c12041cfc6f42bc1f45ed2c67d
Nashalys Fernandez <nashalysf@gmail.com>
2023-06-08 17:43:04 -0400

    fixing new dictionary value

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -2,5 +2,5 @@
 
 def multiply_by_2(a_dictionary):
     new_d = a_dictionary.copy()
-    new_d = ([[(x * 2) for x in key] for key in new_d])
+    new_d = {key: (value * 2) for (key, value) in new_d.items()}
     return (new_d)
