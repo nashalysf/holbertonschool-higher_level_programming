0d8b35a7c0ec63b4b2c7dc697513ad8d5bf648a6
Nashalys Fernandez <nashalysf@gmail.com>
2023-06-14 14:14:10 -0400

    defines a square by

---
+++ python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+class Square:
+    """Empty class that defines a square
+    Attributes: empty constructor
+    """
+
+    def __init__(self, size):
+        """init method for square
+        """
+        self.__size = size
