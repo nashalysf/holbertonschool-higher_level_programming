aaee4692844dd24d04415a5c128943a2ed04773b
Nashalys Fernandez <nashalysf@gmail.com>
2023-06-14 22:53:21 -0400

    fixing module commenting

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -3,7 +3,10 @@
 """
 Python OOP: classes
 """
+
+
 class Square:
+
     """class that defines a square
     Attributes: 
         size: size of square
@@ -12,6 +15,7 @@ class Square:
     def __init__(self, size):
         """init method for square
         Args:
-            private instance size
+        private instance size
         """
+
         self.__size = size
