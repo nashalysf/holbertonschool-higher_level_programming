e10b3d33b891e9a2f6089b58b320b5fe25066210
Nashalys Fernandez <nashalysf@gmail.com>
2023-06-14 22:47:23 -0400

    empty class

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Python OOP: more classes
+"""
+
+
+class Rectangle:
+    """Empty Rectangle class
+    Attributes:
+        None
+    """
+    pass
