b3999105b0ae3f22b0a73626ecea4c32b95158aa
Nashalys Fernandez <nashalysf@gmail.com>
2023-06-14 23:27:13 -0400

    fixing typo

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -15,7 +15,8 @@ class Rectangle:
         """_summary_
 
         Args:
-            width (_type_): _description_
+            width (int)
+            height (int)
         """
 
         self.__width = width
@@ -29,7 +30,7 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """SETTER: sets width of rectangle"""
-        if value is not int:
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
@@ -44,7 +45,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """SETTER: sets height of rectangle"""
-        if value is not int:
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
