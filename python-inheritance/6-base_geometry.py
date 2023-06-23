576315bb95a2eba66234540e20bc4704580aec64
Nashalys Fernandez <nashalysf@gmail.com>
2023-06-22 23:29:24 -0400

    raises exception

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Python: Object Inheritance
+"""
+
+class BaseGeometry:
+    
+    """
+    Base Geometry class
+
+    Public Instance
+    """
+    
+    pass
+
+    def area(self):
+        raise Exception("area() is not implemented")
