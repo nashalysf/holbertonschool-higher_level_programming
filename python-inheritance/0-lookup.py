46502ee4454868fa11991bf6d1b7414e6b871e8f
Nashalys Fernandez <nashalysf@gmail.com>
2023-06-22 22:58:04 -0400

    returns list of obj methods and attributes

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Python: Object Inheritance
+"""
+
+def lookup(object):
+    """
+    Returns the list of available attributes and methods of object
+    """
+
+    return dir(object)
