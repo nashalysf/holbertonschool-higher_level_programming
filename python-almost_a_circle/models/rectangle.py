be0a0f5e7466e601b4ffd363f887831cc4b2e39d
Nashalys Fernandez <nashalysf@gmail.com>
2023-06-29 18:01:38 -0400

    adding display method

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -81,3 +81,8 @@ class Rectangle(Base):
     def area(self):
         """Return the area of the rectangle"""
         return self.__width * self.__height
+
+    def display(self):
+        """Display the rectangle"""
+        for i in range(self.__height):
+            print(" " * self.__x + "#" * self.__width)
