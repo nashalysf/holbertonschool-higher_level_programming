715f7f3e630cfd1952b7c79ba6d25c5d968ede42
Nashalys Fernandez <nashalysf@gmail.com>
2023-06-29 18:40:17 -0400

    adding setter

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -15,6 +15,16 @@ class Square(Rectangle):
         """Return the size of the square"""
         return self.width
 
+    @size.setter
+    def size(self, value):
+        """Size setter"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
+        self.width = value
+        self.height = value
+
     def __str__(self):
         """Return a string representation of Square"""
         return ("[Square] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y, self.width))
