b0f3f360541c86312346943c38c5ba944df9bb86
Nashalys Fernandez <nashalysf@gmail.com>
2023-06-29 18:16:32 -0400

    adding update method

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -92,3 +92,16 @@ class Rectangle(Base):
     def __str__(self):
         """Returns a string representation of the rectangle"""
         return ("[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y, self.__width, self.__height))
+
+    def update(self, *args):
+        for i in args:
+            if i == 0:
+                self.id = args[i]
+            elif i == 1:
+                self.__width = args[i]
+            elif i == 2:
+                self.__height = args[i]
+            elif i == 3:
+                self.__x = args[i]
+            elif i == 4:
+                self.__y = args[i]
