f664c8d0d93da3f4a6d7874150afbf462c644963
Nashalys Fernandez <nashalysf@gmail.com>
2023-06-29 19:06:39 -0400

    adding the json file method

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -14,8 +14,27 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """Returns JSON representation of a list of dictionaries"""
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
-        return json.dumps(list_dictionaries)
+        else:
+            return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Writes JSON string to file"""
+        if list_objs is None or len(list_objs) == 0:
+            list_objs = []
+
+        tmp_list = []
+        filename = cls.__name__ + '.json'
+
+        with open(filename, 'w') as file:
+            """iterate over list"""
+            for i in list_objs:
+                tmp_list.append(i.to_dictionary())
+            """write on file"""
+            data = Base.to_json_string(tmp_list)
+            file.write(data)
