3958a52458f585ba0774ead5c8bef2632cd937b6
Nashalys Fernandez <nashalysf@gmail.com>
2023-06-29 18:08:13 -0400

    adding string method

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -89,4 +89,4 @@ class Rectangle(Base):
 
     def __str__(self):
         """Returns a string representation of the rectangle"""
-        return ("[Rectangle] ({}) {}/{} - {}/{}".format(self.__id, self.__x, self.__y, self.__width, self.__height))
+        return ("[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y, self.__width, self.__height))
