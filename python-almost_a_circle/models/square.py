6c635ce5ec835caca9246057c5525ea387b1a1fa
Nashalys Fernandez <nashalysf@gmail.com>
2023-06-29 18:42:16 -0400

    fixing string method

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -27,4 +27,4 @@ class Square(Rectangle):
 
     def __str__(self):
         """Return a string representation of Square"""
-        return ("[Square] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y, self.width))
+        return ("[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width))
