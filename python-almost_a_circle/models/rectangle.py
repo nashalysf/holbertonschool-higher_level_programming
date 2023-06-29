04faeefd0f11fbf60f2d816266cc447956f194f0
Nashalys Fernandez <nashalysf@gmail.com>
2023-06-29 17:26:05 -0400

    rectangle class getters typo

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -23,8 +23,9 @@ class Rectangle(Base):
     def width(self, value):
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        if value <= 0:
+        elif value <= 0:
             ValueError("width must be > 0")
+        self.__width = value
 
     @property
     def height(self):
@@ -35,8 +36,9 @@ class Rectangle(Base):
     def height(self, value):
         if type(value) is not int:
             raise TypeError("height must be an integer")
-        if value <= 0:
+        elif value <= 0:
             ValueError("height must be > 0")
+        self.__height = value
 
     @property
     def x(self):
@@ -47,8 +49,9 @@ class Rectangle(Base):
     def x(self, value):
         if type(value) is not int:
             raise TypeError("x must be an integer")
-        if value <= 0:
+        elif value <= 0:
             ValueError("x must be >= 0")
+        self.__x = value
 
     @property
     def y(self):
@@ -59,5 +62,6 @@ class Rectangle(Base):
     def y(self, value):
         if type(value) is not int:
             raise TypeError("y must be an integer")
-        if value <= 0:
+        elif value <= 0:
             ValueError("y must be >= 0")
+        self.__y = value
