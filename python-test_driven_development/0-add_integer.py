3db3e2cfcbe0e2212ac3254b48a277eec64da90b
Nashalys Fernandez <nashalysf@gmail.com>
2023-07-01 16:27:21 -0400

    fixing exceptions and return

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -10,10 +10,10 @@ def add_integer(a, b=98):
     Function: add 2 ints
     """
     if type(a) is not int:
-        raise TypeError("a must be an integer or b must be an integer")
+        raise TypeError("a must be an integer")
     if type(b) is not int:
-        raise TypeError("a must be an integer or b must be an integer")
+        raise TypeError("b must be an integer")
     a_int = int(a)
     b_int = int(b)
 
-    return (a + b)
+    return (a_int + b_int)
